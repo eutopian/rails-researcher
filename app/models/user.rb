eda6925d4a57c1bdd7e08673e92e816a5857d563
Margaret <eutopia@gmail.com>
2016-09-16 17:13:48 -0400

    added some attributes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+	before_save { self.email = email.downcase, self.first_name = first_name.name_format, self.last_name = last_name.name_format}
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6, maximum: 30 }
+
+protected
+	def name_format(name)
+		name.downcase
+		([name.split("").first.upcase] << [(name.split("").drop(1)).join("")]).join
+	end
 end
