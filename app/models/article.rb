bc6aa87045cb4e8249fd097fff5e09d1ef3b31fe
Margaret <eutopia@gmail.com>
2016-09-20 15:46:01 -0400

    added comments, attempting to fix associations with comments and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,10 +2,11 @@ class Article < ApplicationRecord
 	belongs_to :author, :class_name => "User"
 	belongs_to :topic
 	has_many :reviews
-	has_many :comments
+	has_many :comments, :inverse_of => :article
 	accepts_nested_attributes_for :topic, reject_if: :all_blank
+	accepts_nested_attributes_for :comments, reject_if: :all_blank
 
-	has_attached_file :document
+	has_attached_file :document, :default_url => "missing.png"
  	validates_attachment :document, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 
 end
