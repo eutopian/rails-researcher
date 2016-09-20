3ab7f1f1074d692a4ef4d2c650d3e821a73123c3
Margaret <eutopia@gmail.com>
2016-09-19 21:53:45 -0400

    added upload function to article

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :twitter, ENV.fetch('CONSUMER_KEY'), ENV.fetch('CONSUMER_SECRET')
   provider :facebook, ENV.fetch('FACEBOOK_KEY'), ENV.fetch('FACEBOOK_SECRET')
 end
