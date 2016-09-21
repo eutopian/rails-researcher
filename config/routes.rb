c43c18fcffc82b8184dffca96bedb47e6709d83d
Margaret <eutopia@gmail.com>
2016-09-21 12:36:15 -0400

    moved commets_attributes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new', as: 'signup'
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#destroy'
-  # delete '/articles/:id' => 'articles#destroy', as: ''
+  # get '/comments/:id/edit' => 'comments#update'
+  # post '/comments/:id' => 'articles#show'
+
   get '/auth/facebook/callback' => 'sessions#create'
   get '/auth/twitter/callback' => 'sessions#create'
   get '/auth/google_oauth2/callback' => 'sessions#create'
